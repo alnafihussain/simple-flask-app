55012aa34284c009f66c68f650898c1d4d47cd93
alnafihussain <156410967+alnafihussain@users.noreply.github.com>
2024-01-12 13:30:15 +0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,4 @@
-image=hussain14/flask-app
+image=hussainalnafi/flask-app
 echo "Stopping and Deleting Container based on image $image"
 docker rm -f $(docker ps -aqf ancestor=$image)
 echo "Deleted Container. Now Removing Image $image"
