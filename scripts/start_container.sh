7d88ca0144a9c87327bcee73cc86ef091d51d56b
alnafihussain <156410967+alnafihussain@users.noreply.github.com>
2024-01-12 13:30:03 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,5 @@
 port=80
-image=hussain14/flask-app
+image=hussainalnafi/flask-app
 echo "Starting Container on port $port"
 docker pull $image
 docker run -dp 80:3000 $image && echo "Successfully started container"
