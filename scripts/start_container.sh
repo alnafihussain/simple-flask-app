19d9d1085336247e94b2bd1481bae1b8ff29f56e
alnafihussain <156410967+alnafihussain@users.noreply.github.com>
2024-01-12 19:06:38 +0500

    update app.py

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,7 @@
-port=80
 image=hussainalnafi/flask-app
+
+port=80
+
 echo "Starting Container on port $port"
 docker pull $image
 docker run -dp 80:3000 $image && echo "Successfully started container"
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,5 @@
 image=hussainalnafi/flask-app
+
 echo "Stopping and Deleting Container based on image $image"
 docker rm -f $(docker ps -aqf ancestor=$image)
 echo "Deleted Container. Now Removing Image $image"
